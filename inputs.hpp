433d102336bb933be4d844594e63a56d04e66579
kernelk14 <khyleisaias@gmail.com>
2025-09-15 17:47:19 +0800

    inputs.hpp: Changed the way how the function gets value

--- inputs.hpp
+++ inputs.hpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
 using namespace std;
+
 string GetString(string prompt) {
     string str_res;
-
     cout << prompt;
-    cin >> str_res;
-
+    getline(cin, str_res); 
     return str_res;
+}
+
+int getInt(string prompt) {
+    int int_res;
+    cout << prompt;
+    cin >> int_res;
+    return int_res;
 }
